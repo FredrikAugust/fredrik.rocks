1849fcc4a728049a82b1de10563ae8743426c6ba
Fredrik Malmo <fredrik.malmo@icloud.com>
2021-02-09 12:44:15 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/script.ts",
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanAfterEveryBuildPatterns: ["./docs"],
+    }),
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
+  output: {
+    path: __dirname + "/docs",
+    filename: "[name].[contenthash].js",
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [{ test: /\.tsx?$/, loader: "ts-loader" }],
+  },
+};
